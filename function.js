ad2b1c2afe1a313f3d14e1f363f0b797205fab22
Your Name <you@example.comcd>
2026-01-07 16:29:40 +0600

    simple ticket price calculation

--- function.js
+++ function.js
@@ -1,32 +1,31 @@
-//function k ekta hoto factory ba machine hishebe cint kor mone koro eta ekta juice blender 
-//data flow 3 ta dhap e hoy 
- /**
-  * 1- input (dhuka)
-  * 2-process (kaj)
-  * 3-output (ber howa)
-  * 4-{} function er vhitor curly braces er vhitor ami jai dei er bahire keo dekhbe na (secret recipe)
-  * exmple: function makeJuice(fruits{
-  * water = "sugar water"}- etai muloto 4 number e bujiyechi
-  * 5-output ba return key word ta holo delivery boy
-  * jodi ami return na likhi tahole function kaj korbe thiki but amk bahire (function ) kichu dibe na(result hobe undefined) 
+//function k ekta hoto factory ba machine hishebe cint kor mone koro eta ekta juice blender
+//data flow 3 ta dhap e hoy
+/**
+ * 1- input (dhuka)
+ * 2-process (kaj)
+ * 3-output (ber howa)
+ * 4-{} function er vhitor curly braces er vhitor ami jai dei er bahire keo dekhbe na (secret recipe)
+ * exmple: function makeJuice(fruits{
+ * water = "sugar water"}- etai muloto 4 number e bujiyechi
+ * 5-output ba return key word ta holo delivery boy
+ * jodi ami return na likhi tahole function kaj korbe thiki but amk bahire (function ) kichu dibe na(result hobe undefined)
  */
 //bus ticket er data flow dekhi function ta use kore
 
-
 //1-factory design
-function calculateTotal (quantity){
-      //input flow 
-      //bahire theke data ashlo and quantity variable e boshlo
+function calculateTotal(quantity) {
+  //input flow
+  //bahire theke data ashlo and quantity variable e boshlo
 
-      //[process flow]
-      const pricePerTicket = 550; //local variable(secret-recipe)
-      const total = quantity*pricePerTicket; // math hocche
+  //[process flow]
+  const pricePerTicket = 550; //local variable(secret-recipe)
+  const total = quantity * pricePerTicket; // math hocche
 
-      //[Output Flow]
-      return total;
+  //[Output Flow]
+  return total;
 }
 //step-2: Factory use (Function all)----
-//1 . amra 3 pathalam 
+//1 . amra 3 pathalam
 //2 . 3 gelo quantity er kache
 //3. calculation holo (3*550 = 1650).
 // 4 . 1650 return hoye my bill er moddhe boshlo
@@ -36,8 +35,8 @@ console.log(myBill);
 /**
  * 1-mistake the undefined trap
  * data flow bujhar sobceye boro mistake holo return na kora .
- * 
-*/
+ *
+ */
 
 // function wrongClac (num){
 //       let result = num*2;
@@ -46,3 +45,33 @@ console.log(myBill);
 // let output = wrongClac(5);
 // console.log(output);
 
+function makeBurger(quantity) {
+  const burger = 120;
+  if (quantity > 5) {
+//     console.log("only 5 burger allow to purchase")
+    return "sorry max 5 burger allowed";
+  }else{
+      let totalPrice = burger*quantity;
+      return totalPrice;
+}
+}
+ let output = makeBurger(6);
+ console.log(output);
+
+
+ function calculateBusFare(quantity,isBusiness){
+      const economy = 550;
+      const business = 800;
+      if (isBusiness === true){
+            let total = quantity * business;
+            return total;
+      }else{
+            let total = quantity*economy;
+            return total;
+      }
+ }
+
+ let test1 = calculateBusFare(4 , true);
+ console.log("test 1 cost :", test1);
+ let test2 = calculateBusFare(2 , false);
+ console.log("test 2 total", test2);
